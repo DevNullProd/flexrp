66c18b14c64ac3b9918c42405462c7324aec377a
flyfoxuk <1914280+flyfoxuk@users.noreply.github.com>
2021-03-04 22:31:10 +0000

    bounty: secp256k1 and ed25519 private key ability

--- js/network.js
+++ js/network.js
@@ -27,6 +27,25 @@ async function sign_tx(api, settings){
   var xrp_secret = document.getElementById("xrp_secret");
   var xrp_address = document.getElementById("xrp_address");
   var eth_address = document.getElementById("eth_address")
+  var keyPair = {}
+
+  // generate the keyPair to use for signing purposes, 
+  // either from the private key, or the secret, 
+  // depending on what was specified
+  if (offline_api.isValidSecret(xrp_secret.value)) {
+    // a secret key was specified
+    keyPair = offline_api.deriveKeypair(xrp_secret.value);
+  } else {
+    // a private key was specified (either 64 chars, or 66 chars
+    // as either secp256k1 [prefix 00] or ed255519 [prefix ED])
+    const privateKey = xrp_secret.value.length == 64 ? '00' + xrp_secret.value : xrp_secret.value
+    keyPair = {
+        privateKey: privateKey,
+        publicKey:  privateKey.slice(2) == 'ED' 
+                    ? 'ED' + bytesToHex(ed25519.keyFromSecret(privateKey.slice(2)).pubBytes())
+                    : bytesToHex(secp256k1.keyFromPrivate(privateKey.slice(2)).getPublic().encodeCompressed())
+    }
+  }
 
   const instructions = settings.offline ? {
     fee : settings.fee,
@@ -37,14 +56,14 @@ async function sign_tx(api, settings){
   // Specified xrp address or convert secret to public address
   const xrp_addr = settings.specify_account ?
     xrp_address.value :
-    offline_api.deriveAddress(offline_api.deriveKeypair(xrp_secret.value).publicKey);
+    offline_api.deriveAddress(keyPair.publicKey);
 
   // Eth address to message key
   const message_key = ('02' + (new Array(25).join("0")) + eth_address.value.substr(2)).toUpperCase()
 
   // Create AccountSet transaction, setting the message key, and sign
   const prepared = await api.prepareSettings(xrp_addr, {messageKey: message_key}, instructions)
-  return api.sign(prepared.txJSON, xrp_secret.value)
+  return api.sign(prepared.txJSON, keyPair)
 }
 
 // Process transaction
