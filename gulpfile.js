37a7a3490ab0221b2f0cee281c72f5f44640ab16
lathan britz <lathan.britz@drupalconnect.com>
2021-03-11 00:22:25 -0300

    Setup gulp (cheers), so can start using sass frameworks. All SASS files are simply a clone of CSS at this moment.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var cleanCss = require("gulp-clean-css");
+
+//compile scss into css
+function style() {
+  return gulp
+    .src("scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(cleanCss())
+    .pipe(gulp.dest("css"));
+}
+exports.style = style;
+
+function watch() {
+  gulp.watch("scss/**/*.scss", style);
+}
+exports.watch = watch;
+
+exports.default = watch;
